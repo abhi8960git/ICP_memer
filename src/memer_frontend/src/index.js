d8e15514ee77515b884e9e2be831c574cfeb873e
deauth <anurage66@gmail.com>
2023-12-03 22:44:07 +0530

    completed mint

--- src/memer_frontend/src/index.js
+++ src/memer_frontend/src/index.js
@@ -1,60 +1,91 @@
 import { memera } from "../../declarations/memera/";
+import { Principal } from "@dfinity/principal";
 
+document
+	.querySelector("#mintForm")
+	.addEventListener("submit", async (event) => {
+		event.preventDefault();
 
-const Button = document.getElementsByClassName('myButton');
-console.log(Button);
+		const button = event.target.querySelector("button");
+		const mintText1 = document.getElementById("mintText1").value;
+		const mintText2 = document.getElementById("mintText2").value;
+		const uint8Array = stringToUint8Array(mintText1);
 
-Button[0].addEventListener('click', function() {
-  console.log("hello");
-});
+		function stringToUint8Array(str) {
+			const values = str.split(";");
+			const uint8Array = new Uint8Array(values.length);
 
+			for (let i = 0; i < values.length; i++) {
+				uint8Array[i] = parseInt(values[i]);
+			}
 
+			return uint8Array;
+		}
 
+		const nftArray = convertUint8ArrayToVecNat16(uint8Array);
 
+		function convertUint8ArrayToVecNat16(uint8Array) {
+			const nat16Array = new Uint16Array(uint8Array.length / 2);
 
-const mintForm = document.getElementById('mintForm');
-mintForm.addEventListener('submit', async (event) => {
-  event.preventDefault();
+			for (let i = 0; i < uint8Array.length; i += 2) {
+				const nat16Value = (uint8Array[i] << 8) | uint8Array[i + 1];
+				nat16Array[i / 2] = nat16Value;
+			}
 
-  const button = event.target.querySelector("button");
-  const mintText1 = document.getElementById('mintText1').value;
-  const mintText2 = document.getElementById('mintText2').value;
-  const uint8Array = stringToUint8Array(mintText1);
+			return nat16Array;
+		}
 
-  function stringToUint8Array(str) {
-    const values = str.split(';');
-    const uint8Array = new Uint8Array(values.length);
-  
-    for (let i = 0; i < values.length; i++) {
-      uint8Array[i] = parseInt(values[i]);
-    }
-  
-  return uint8Array;
-  }
+		button.setAttribute("disabled", true);
 
- const nftArray= convertUint8ArrayToVecNat16(uint8Array);
+		// Call the memara.Mint() function with mintText1 and mintText2 as arguments
+		const principal = await memera.Mint(nftArray, mintText2);
 
-  function convertUint8ArrayToVecNat16(uint8Array) {
-    const nat16Array = new Uint16Array(uint8Array.length / 2);
-  
-    for (let i = 0; i < uint8Array.length; i += 2) {
-      const nat16Value = (uint8Array[i] << 8) | uint8Array[i + 1];
-      nat16Array[i / 2] = nat16Value;
-    }
-  
-    return nat16Array;
-  }
+		button.removeAttribute("disabled");
+		console.log(principal);
 
+		// document.getElementById('mintResult').innerText = `Principal: ${principal}`;
+	});
 
+//get your id
+document
+	.querySelector("#getYourIdForm")
+	.addEventListener("submit", async (e) => {
+		e.preventDefault();
+		const button = e.target.querySelector("button");
 
+		const name = document.getElementById("name").value.toString();
 
-  button.setAttribute("disabled", true);
+		button.setAttribute("disabled", true);
 
-  // Call the memara.Mint() function with mintText1 and mintText2 as arguments
-  const principal = await memera.Mint(nftArray, mintText2);
+		// Interact with foo actor, calling the greet method
+		const myId = await memera.getYourId();
 
-  button.removeAttribute("disabled");
-  console.log(principal);
+		button.removeAttribute("disabled");
 
-  // document.getElementById('mintResult').innerText = `Principal: ${principal}`;
-});
+		console.log(myId);
+
+		return false;
+	});
+
+//isListed
+document
+	.querySelector("#isListedForm")
+	.addEventListener("submit", async (e) => {
+		e.preventDefault();
+		const button = e.target.querySelector("button");
+
+		const listedPrincipal = document
+			.getElementById("listedPrincipalId")
+			.value.toString();
+
+		button.setAttribute("disabled", true);
+
+		// Interact with foo actor, calling the greet method
+		const isListed = await memera.isListed(Principal.fromText(listedPrincipal));
+
+		button.removeAttribute("disabled");
+
+		console.log(isListed);
+
+		return false;
+	});
